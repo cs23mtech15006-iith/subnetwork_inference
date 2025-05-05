816b063324bfaa0e4428026e62ebbd91d1941faa
cs23mtech15006-iith <cs23mtech15006@iith.ac.in>
2025-05-06 02:40:08 +0530

    Cleanup (#3)
    * cleanup - removed unused functions and files
    
    * updated plots.ipynb
    
    * updated notebooks, added results, removed environment.yml, updated gitignore

--- src/datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-# __all__ = ['load_axis', 'load_origin', 'load_agw_1d', 'load_andrew_1d',
-#            'load_flight', 'load_gap_UCI', 'gen_spirals']
-
-# from datasets.additional_gap_loader import load_axis, load_origin, load_agw_1d, load_andrew_1d
-# from datasets.flight_delay_loader import load_flight
-# from datasets.UCI_gap_loader import load_gap_UCI
-# from datasets.spirals_gen import gen_spirals
